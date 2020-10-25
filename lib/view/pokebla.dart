dd41f2277ac425b361a4ccc14de8d6d3dac54464
Arthur Rodrigues <arthursmr17@gmail.com>
2020-10-25 19:57:58 -0300

    navigation bar title at pokedex

--- lib/view/pokebla.dart
+++ lib/view/pokebla.dart
@@ -1,23 +1,34 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'pokemonCell.dart';
 
 class PokeblaView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Padding(
-      padding: EdgeInsets.all(16),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(8),
+    return CupertinoPageScaffold(
+      child: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            CupertinoSliverNavigationBar(
+              largeTitle: Text('Pokedex'),
+              backgroundColor: Colors.transparent,
+            )
+          ];
+        },
+        body: Center(
+          child: PokemonCellView.build(
+              context,
+              'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png',
+              "Pikachu",
+              "Elétrico"),
         ),
-        child: PokemonCellView.build(
-            context,
-            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png',
-            "Pikachu",
-            "Elétrico"),
       ),
-    ));
+    );
   }
 }
+
+// child: PokemonCellView.build(
+//     context,
+//     'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png',
+//     "Pikachu",
+//     "Elétrico"),
--- lib/view/pokemonCell.dart
+++ lib/view/pokemonCell.dart
@@ -1,43 +1,51 @@
 import 'package:flutter/material.dart';
 
 class PokemonCellView {
-  static ListTile build(BuildContext context, String imageURL,
+  static Scaffold build(BuildContext context, String imageURL,
       String pokemonName, String pokemonType) {
-    return ListTile(
-      onTap: () {},
-      selected: true,
-      trailing: Icon(
-        Icons.keyboard_arrow_right,
-        color: Colors.grey,
-      ),
-      leading: Container(
-        width: 64,
-        height: 64,
+    return Scaffold(
+      body: Container(
         decoration: BoxDecoration(
-          color: Colors.transparent,
-          image: DecorationImage(
-            image: NetworkImage(imageURL),
-          ),
-          borderRadius: BorderRadius.circular(32.0),
-          border: Border.all(
-            color: Colors.black,
-            width: 0.5,
-          ),
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
         ),
-      ),
-      title: Text(
-        pokemonName,
-        style: TextStyle(
-          fontSize: 18.0,
-          color: Colors.black,
-        ),
-      ),
-      subtitle: Text(
-        pokemonType,
-        overflow: TextOverflow.ellipsis,
-        style: TextStyle(
-          fontSize: 16.0,
-          color: Colors.grey,
+        child: ListTile(
+          onTap: () {},
+          selected: true,
+          trailing: Icon(
+            Icons.keyboard_arrow_right,
+            color: Colors.grey,
+          ),
+          leading: Container(
+            width: 64,
+            height: 64,
+            decoration: BoxDecoration(
+              color: Colors.transparent,
+              image: DecorationImage(
+                image: NetworkImage(imageURL),
+              ),
+              borderRadius: BorderRadius.circular(32.0),
+              border: Border.all(
+                color: Colors.black,
+                width: 0.5,
+              ),
+            ),
+          ),
+          title: Text(
+            pokemonName,
+            style: TextStyle(
+              fontSize: 18.0,
+              color: Colors.black,
+            ),
+          ),
+          subtitle: Text(
+            pokemonType,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontSize: 16.0,
+              color: Colors.grey,
+            ),
+          ),
         ),
       ),
     );
